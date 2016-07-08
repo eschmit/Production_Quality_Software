c12fed5b3aa595b883dbe68bda74514cb12404c3
Eric Schmitterer <eschmit91@gmail.com>
2016-07-08 16:23:53 -0400

    code review

--- PS3/testsrc/addressbook/AddressBookTest.java
+++ PS3/testsrc/addressbook/AddressBookTest.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
